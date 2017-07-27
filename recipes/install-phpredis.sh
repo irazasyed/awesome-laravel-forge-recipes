c806c812d0725bd83c53800dac1846abcd0ec8f3
Syed Irfaq R. ⚡️ <irazasyed@users.noreply.github.com>
2017-07-28 04:55:14 +0530

    Add Install-PHPRedis Recipe

---
+++ recipes/install-phpredis.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Set PHP Version
+PHP_VERSION=7.1
+
+# Clone
+printf "\n"
+printf "Cloning PHPRedis...\n"
+printf "PHP Version: $PHP_VERSION\n"
+
+cd /tmp
+git clone https://github.com/phpredis/phpredis phpredis
+cd phpredis
+
+# Install
+printf "Installing PHPRedis...\n"
+
+phpize
+./configure
+sudo make
+sudo make install
+
+# Configure
+printf "Configuring PHPRedis!\n"
+
+echo "extension=redis.so" > sudo nano /etc/php/$PHP_VERSION/mods-available/redis.ini
+sudo ln -s /etc/php/$PHP_VERSION/mods-available/redis.ini /etc/php/$PHP_VERSION/fpm/conf.d/30-redis.ini
+sudo ln -s /etc/php/$PHP_VERSION/mods-available/redis.ini /etc/php/$PHP_VERSION/cli/conf.d/30-redis.ini
+
+# Restart
+printf "Restarting PHP!\n"
+sudo service php$PHP_VERSION-fpm restart
